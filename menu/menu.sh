228e491e16fdcc60da98902972f07455ca276218
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-07-11 16:18:05 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -158,9 +158,10 @@ echo -e "\e[1;36m 1 \e[0m: Menu SSH                \e[1;36m 6 \e[0m: Menu Settin
 echo -e "\e[1;36m 2 \e[0m: Menu Vmess              \e[1;36m 7 \e[0m: Status Service"
 echo -e "\e[1;36m 3 \e[0m: Menu Vless              \e[1;36m 8 \e[0m: Clear RAM Cache"
 echo -e "\e[1;36m 4 \e[0m: Menu Trojan             \e[1;36m 9 \e[0m: Reboot VPS"
-echo -e "\e[1;36m 5 \e[0m: Menu Shadowsocks        \e[1;36m x \e[0m: Exit Script"
-echo -e   ""
-echo -e "\e[1;33m ---------------------------------------------------\e[0m"
+echo -e "\e[1;36m 5 \e[0m: Menu Shadowsocks        \e[1;36m 10 \e[0m: Backup/Restore"                                     
+echo -e "\e[1;33m -----------------------------------------------------\e[0m"
+echo -e "\e[1;34m            x \e[0m:Press (x) To Exit Script"    
+echo -e "\e[1;33m -------------------------------------------------------\e[0m"
 echo -e "\e[1;32m Client Name \e[0m: $Name"
 echo -e "\e[1;32m Expired     \e[0m: $REMAINING_STATUS"
 echo -e "\e[1;33m ---------------------------------------------------\e[0m"
@@ -181,6 +182,7 @@ case $opt in
 6) clear ; m-system ;;
 7) clear ; running ;;
 8) clear ; clearcache ;;
+10) clear ; m-bkp ;;
 9) clear ; reboot ; /sbin/reboot ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu ;;
